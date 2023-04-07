f84a4c542607e967117acfce97ec8324aba46300
jone4865 <cowhddnjsdl0@naver.com>
2023-04-07 09:36:39 +0900

    fix: 네이버 검색엔진 노출 오류 수정중

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -97,7 +97,7 @@ export default function Footer() {
           </div>
           <div className={styles.footer_line3_left}>
             <div className={styles.footer_line3_title}>고객센터</div>
-            <div onClick={onClickTell}>고객상담 : 1833-8604</div>
+            <a href="tel:010-1234-5678">010-1234-5678</a>
             <div>점심시간 : 13:00 ~ 14:00</div>
             <div>Email: realfiexchnge@gmail.com</div>
           </div>
