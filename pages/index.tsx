88fd62c4e066174e25384b1177bf49e25d84d58b
jone4865 <cowhddnjsdl0@naver.com>
2023-04-12 10:17:34 +0900

    fix: index.tsx next/head 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import { Inter } from "@next/font/google";
-import Head from "next/head";
 import Landing from "../components/Landing/Landing";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -7,24 +6,6 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <Head>
-        <meta name="Keywords" content="realfi" />
-        <meta name="Keywords" content="realfiex" />
-        <meta property="og:url" content="http://realfiex.com/" />
-        <meta property="og:title" content="Real_Fi" />
-        <meta
-          property="og:description"
-          content="소액으로 시작하는 부동산 조각 투자"
-        />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          property="og:article:author"
-          content="소액으로 시작하는 부동산 조각 투자"
-        />
-        <meta property="og:image" content="/img/logo/og_img.png" />
-        <meta property="og:image:width" content="800" />
-        <meta property="og:image:height" content="400" />
-      </Head>
       <Landing />
     </>
   );
